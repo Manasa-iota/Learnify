ff687742e4793ee0dc250c2b0e9c163de14d6059
Manasa <119737682+ManasaNagaram@users.noreply.github.com>
2025-02-22 12:48:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,11 +23,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))//for using postman
 app.use(cookieParser());
 app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
-)
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
+
 
 app.use(
 	fileUpload({
