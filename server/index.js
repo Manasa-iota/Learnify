9f5306d3257bf23d69baaeb1e4c8707d742a32e5
Manasa <119737682+ManasaNagaram@users.noreply.github.com>
2025-02-22 16:27:18 +0530

    Add alternate url

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({extended: true}))//for using postman
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL  || process.env.ALTERNATE_URL || "http://localhost:3000",
     credentials: true,
   })
 );
